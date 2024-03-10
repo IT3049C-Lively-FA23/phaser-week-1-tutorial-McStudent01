924380b08815307414b8ec5ce31faf4e2817658c
McStudent01 <mcmillcy@mail.uc.edu>
2024-03-10 13:26:06 -0500

    added powerup sprite image and physics logic

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
-    var config = {
-        width: 256,
-        height: 272,
-        backgroundColor: 0x000000,
-        scene: [Scene1, Scene2],
-        pixelArt: true
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade:{
+          debug: false
       }
-      
-      var game = new Phaser.Game(config);
+    }
+  }
+  
+  var game = new Phaser.Game(config);
